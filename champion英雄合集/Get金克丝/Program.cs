c821abdbe2fe43cfb9521c376d766a84e57f513c
ergou <654955321@qq.com>
2014-12-29 17:32:42 +0800

    增加Daimao蒙多    Get金克丝    Lightning光辉   Lightning瑞兹   Obama_雷欧娜   Prunes易大师   yetAnother皎月    yetAnother乌迪尔

---
+++ champion英雄合集/Get金克丝/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+using Color = System.Drawing.Color;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace GetJinxed
+{
+    internal class program
+    {
+        private const string Champion = "Jinx";
+        private static Orbwalking.Orbwalker Orbwalker;
+        private static Spell Q;
+        private static Spell W;
+        private static Spell E;
+        private static Spell R;
+        private static List<Spell> SpellList = new List<Spell>();
+        private static Menu Config;
+        public static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            if (ObjectManager.Player.BaseSkinName != Champion) return;
+
+            Q = new Spell(SpellSlot.Q);
+            W = new Spell(SpellSlot.W, 1500f);
+            E = new Spell(SpellSlot.E, 900f);
+            R = new Spell(SpellSlot.R, 25000f);
+
+            W.SetSkillshot(0.6f, 60f, 3300f, true, SkillshotType.SkillshotLine);
+            E.SetSkillshot(0.7f, 120f, 1750f, false, SkillshotType.SkillshotCircle);
+            R.SetSkillshot(0.6f, 140f, 1700f, false, SkillshotType.SkillshotLine);
+            SpellList.Add(Q);
+            SpellList.Add(W);
+            SpellList.Add(E);
+            SpellList.Add(R);
+
+            Config = new Menu("Get金克丝", "Sesi0", true);
+
+            var targetSelectorMenu = new Menu("目标 选择", "Target Selector");
+            TargetSelector.AddToMenu(targetSelectorMenu);
+            Config.AddSubMenu(targetSelectorMenu);
+
+            Config.AddSubMenu(new Menu("走 砍", "Orbwalking"));
+            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
+
+            Config.AddSubMenu(new Menu("连 招", "Combo"));
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseWCombo", "使用 W")).SetValue(true);
+            Config.SubMenu("Combo").AddItem(new MenuItem("UseECombo", "使用 E")).SetValue(true);
+            Config.SubMenu("Combo").AddItem(new MenuItem("ActiveCombo", "开启!").SetValue(new KeyBind(32, KeyBindType.Press)));
+
+            Config.AddSubMenu(new Menu("骚 扰", "Harass"));
+            Config.SubMenu("Harass").AddItem(new MenuItem("UseWHarass", "使用 W")).SetValue(true);
+            Config.SubMenu("Harass").AddItem(new MenuItem("ActiveHarass", "开启!").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
+
+
+            Config.AddToMainMenu();
+            Game.OnGameUpdate += OnGameUpdate;
+            Game.PrintChat("<font color='#7FFF00'>GetJinxed by Sesi0 If you liked my assembly please give me a reputation point.</font>Loaded!");
+            Game.PrintChat("<font color='#FF0000'>Get 閲戝厠涓濅辅鍔犺級鎴愬姛锛佹饥鍖朾y 鑺辫竟锛丵Q缇361630847!</font>");
+
+
+        }
+        private static void OnGameUpdate(EventArgs args)
+        {
+            if (Config.Item("ActiveCombo").GetValue<KeyBind>().Active)
+            {
+                Combo();
+            }
+
+        }
+        private static void Combo()
+        {
+            var target = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
+            if (target == null)
+                return;
+            if (W.IsReady() && (Config.Item("UseWCombo").GetValue<bool>()))
+            {
+                W.Cast(target);
+            }
+            if (target.IsValidTarget(E.Range) && E.IsReady() && (Config.Item("UseECombo").GetValue<bool>()))
+            {
+                E.Cast(target, true, true);
+
+            }
+        }
+
+        private static void Harass()
+        {
+            var target = TargetSelector.GetTarget(W.Range, TargetSelector.DamageType.Magical);
+            if (target == null)
+                return;
+            if (W.IsReady() && (Config.Item("UseWHarass").GetValue<bool>()))
+            {
+                W.Cast(target);
+
+            }
+
+        }
+    }
+}
