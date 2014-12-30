3ac02740b6b12b7443ffad13fdecebfb0fe30f45
ergou <654955321@qq.com>
2014-12-30 16:55:59 +0800

    阿狸 阿卡丽 妖姬 光辉

---
+++ ChewyMoons 中单合集/Champions/Ahri.cs
@@ -0,0 +1,177 @@
+﻿#region
+
+using System;
+using System.Drawing;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Mid_or_Feed.Champions
+{
+    internal class Ahri : Plugin
+    {
+        //TODO: Implment some type of Ult logic.
+
+        public Items.Item Dfg;
+        public Spell E;
+        public Spell Q;
+        public Spell W;
+
+        public Ahri()
+        {
+            Q = new Spell(SpellSlot.Q, 1000);
+            W = new Spell(SpellSlot.W, 800);
+            E = new Spell(SpellSlot.E, 1000);
+
+            Q.SetSkillshot(0.25f, 100, 2500, false, SkillshotType.SkillshotLine);
+            E.SetSkillshot(0.25f, 60, 1500, true, SkillshotType.SkillshotLine);
+
+            Dfg = new Items.Item(3128, 750);
+
+            Game.OnGameUpdate += GameOnOnGameUpdate;
+            AntiGapcloser.OnEnemyGapcloser += AntiGapcloserOnOnEnemyGapcloser;
+            Interrupter.OnPossibleToInterrupt += InterrupterOnOnPossibleToInterrupt;
+            Drawing.OnDraw += DrawingOnOnDraw;
+
+            PrintChat("Ahri loaded.");
+        }
+
+        private void DrawingOnOnDraw(EventArgs args)
+        {
+            var drawQ = GetBool("drawQ");
+            var drawW = GetBool("drawW");
+            var drawE = GetBool("drawE");
+            var p = Player.Position;
+
+            if (drawQ)
+                Utility.DrawCircle(p, Q.Range, Q.IsReady() ? Color.Aqua : Color.Red);
+
+            if (drawW)
+                Utility.DrawCircle(p, W.Range, W.IsReady() ? Color.Aqua : Color.Red);
+
+            if (drawE)
+                Utility.DrawCircle(p, E.Range, E.IsReady() ? Color.Aqua : Color.Red);
+        }
+
+        private void InterrupterOnOnPossibleToInterrupt(Obj_AI_Base unit, InterruptableSpell spell)
+        {
+            if (!GetBool("interruptE") || spell.DangerLevel != InterruptableDangerLevel.High)
+                return;
+
+            E.Cast(unit, Packets);
+        }
+
+        private void AntiGapcloserOnOnEnemyGapcloser(ActiveGapcloser gapcloser)
+        {
+            if (!GetBool("gapcloseE"))
+                return;
+
+            E.Cast(gapcloser.Sender, Packets);
+        }
+
+        private void GameOnOnGameUpdate(EventArgs args)
+        {
+            switch (OrbwalkerMode)
+            {
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    DoHarass();
+                    break;
+
+                case Orbwalking.OrbwalkingMode.Combo:
+                    DoCombo();
+                    break;
+            }
+        }
+
+        private void DoCombo()
+        {
+            var target = TargetSelector.GetTarget(1000, TargetSelector.DamageType.Magical);
+
+            if (target == null)
+                return;
+
+            var useQ = GetBool("useQ");
+            var useW = GetBool("useW");
+            var useE = GetBool("useE");
+            var useDfg = GetBool("useDFG");
+
+            if (useDfg && Dfg.IsReady())
+                Dfg.Cast(target);
+
+            if (useE && E.IsReady())
+                E.Cast(target);
+
+            if (useQ && Q.IsReady())
+                Q.Cast(target);
+
+            if (useW && W.IsReady() && W.InRange(target.ServerPosition))
+                W.Cast(Packets);
+        }
+
+        private void DoHarass()
+        {
+            var target = TargetSelector.GetTarget(1000, TargetSelector.DamageType.Magical);
+            if (target == null)
+                return;
+
+            if (!GetBool("useQHarass") || !Q.IsReady())
+                return;
+
+            Q.Cast(target, Packets);
+        }
+
+        public override float GetComboDamage(Obj_AI_Hero target)
+        {
+            double dmg = 0;
+
+            if (Q.IsReady())
+                dmg += Player.GetSpellDamage(target, SpellSlot.Q) + Player.GetSpellDamage(target, SpellSlot.Q, 1);
+
+            if (W.IsReady())
+                dmg += Player.GetSpellDamage(target, SpellSlot.W);
+
+            if (E.IsReady())
+            {
+                dmg += Player.GetSpellDamage(target, SpellSlot.E);
+                dmg += dmg*0.2;
+            }
+
+            if (!Dfg.IsReady()) return (float) dmg;
+            dmg += Player.GetItemDamage(target, Damage.DamageItems.Dfg);
+            dmg += dmg*0.2;
+
+            return (float) dmg;
+        }
+
+        public override void Combo(Menu config)
+        {
+            config.AddItem(new MenuItem("useQ", "使用 Q").SetValue(true));
+            config.AddItem(new MenuItem("useW", "使用 W").SetValue(true));
+            config.AddItem(new MenuItem("useE", "使用 E").SetValue(true));
+        }
+
+        public override void Harass(Menu config)
+        {
+            config.AddItem(new MenuItem("useQHarass", "使用 Q").SetValue(true));
+        }
+
+        public override void ItemMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("useDFG", "使用 冥火").SetValue(true));
+        }
+
+        public override void Misc(Menu config)
+        {
+            config.AddItem(new MenuItem("gapcloseE", "E 防止突进").SetValue(true));
+            config.AddItem(new MenuItem("interruptE", "E 中断法术", true).SetValue(true));
+        }
+
+        public override void Drawings(Menu config)
+        {
+            config.AddItem(new MenuItem("drawQ", "范围 Q").SetValue(true));
+            config.AddItem(new MenuItem("drawW", "范围 W").SetValue(true));
+            config.AddItem(new MenuItem("drawE", "范围 E").SetValue(true));
+        }
+    }
+}
---
+++ ChewyMoons 中单合集/Managers/AutoIgnite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Mid_or_Feed.Managers
+{
+    class AutoIgnite : Manager
+    {
+        private SpellSlot _igniteSlot;
+        private const int IgniteRange = 600;
+
+        private Menu _menu;
+
+        public override void Load(Menu config)
+        {
+            config.AddItem(new MenuItem("igniteKill", "可击杀自动点燃").SetValue(true));
+            config.AddItem(new MenuItem("igniteKS", "使用点燃抢人头").SetValue(true));
+
+            _menu = config;
+            _igniteSlot = ObjectManager.Player.GetSpellSlot("SummonerDot");
+
+            Game.OnGameUpdate += GameOnOnGameUpdate;
+        }
+
+        private void GameOnOnGameUpdate(EventArgs args)
+        {
+            var igniteKill = _menu.Item("igniteKill").GetValue<bool>();
+            var igniteKS = _menu.Item("igniteKS").GetValue<bool>();
+
+            if (!_igniteSlot.IsReady())
+                return;
+
+            if (igniteKill)
+            {
+                foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsEnemy).Where(x => x.Distance(ObjectManager.Player) <= IgniteRange).Where(enemy => ObjectManager.Player.GetSummonerSpellDamage(enemy, Damage.SummonerSpell.Ignite) > enemy.Health))
+                {
+                    ObjectManager.Player.Spellbook.CastSpell(_igniteSlot, enemy);
+                    return;
+                }
+            }
+
+            if (!igniteKS) return;
+            foreach (var enemy in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsEnemy).Where(x => x.Distance(ObjectManager.Player) <= IgniteRange).Where(enemy => enemy.Health <=
+                                                                                                                                                                    ObjectManager.Player.GetSummonerSpellDamage(enemy, Damage.SummonerSpell.Ignite)/5))
+            {
+                ObjectManager.Player.Spellbook.CastSpell(_igniteSlot, enemy);
+                return;
+            }
+        }
+    }
+}
